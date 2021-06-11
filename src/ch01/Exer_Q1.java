6d62bdcb9625f025f85709e4b730f5f60b553fd0
alsrbxo0428 <alsrbxo0428@naver.com>
2021-06-11 17:59:55 +0900

    기본 알고리즘

---
+++ src/ch01/Exer_Q1.java
@@ -0,0 +1,21 @@
+package ch01;
+
+public class Exer_Q1 {
+	//네 값의 최대값을 구하는 max4메서드를 작성하세요.
+	public static int max4(int a, int b, int c, int d) {
+		int max = a;
+		
+		if(b > max)
+			max = b;
+		if(c > max)
+			max = c;
+		if(d > max)
+			max = d;
+		
+		return max;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("max4(4,2,3,1) : " + max4(4,2,3,1));
+	}
+}
---
+++ src/ch01/Exer_Q3.java
@@ -0,0 +1,21 @@
+package ch01;
+
+public class Exer_Q3 {
+	//네 값의 최솟값을 구하는 min4 메서드를 작성하세요.
+	public static int min4(int a, int b, int c, int d) {
+		int min = a;
+		
+		if(b < min)
+			min = b;
+		if(c < min)
+			min = c;
+		if(d < min)
+			min = d;
+		
+		return min;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("min3(2,3,1,4) : " + min4(2,3,1,4));
+	}
+}
