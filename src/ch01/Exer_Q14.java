232703745059d03ea4a0ef8bb7c791a67c72e6c3
alsrbxo0428 <alsrbxo0428@naver.com>
2021-06-13 23:34:32 +0900

    반복문 충돌 문제 해결

--- src/ch01/Exer_Q14.java
+++ src/ch01/Exer_Q14.java
@@ -3,12 +3,12 @@ package ch01;
 import java.util.Scanner;
 
 public class Exer_Q14 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("사각형을 출력합니다.\n단 수 : ");
-		int n = sc.nextInt();
-		for(int i = 0; i < n; i++) {
-			System.out.println("*****");
-		}
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("사각형을 출력합니다.\n단 수 : ");
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++) {
+            System.out.println("*****");
+        }
+    }
 }
