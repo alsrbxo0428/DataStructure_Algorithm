ab28a6a6c16ec8ba35a603545fcda1e16f6e19ca
alsrbxo0428 <alsrbxo0428@naver.com>
2021-06-22 00:59:37 +0900

    2021-06-22 클래스

--- src/ch02/Exer_Q11.java
+++ src/ch02/Exer_Q11.java
@@ -3,6 +3,55 @@ package ch02;
 import java.util.Scanner;
 
 public class Exer_Q11 {
+	public static class YMD {
+		int y;	//년
+		int m;	//월(1 ~ 12)
+		int d;	//일(1 ~ 31)
+		
+		YMD(int y, int m, int d) {//주어진 날짜로 설정
+			this.y = y;
+			this.m = m;
+			this.d = d;
+		}//생성자
+		
+		//n일 뒤의 날짜를 반환
+		YMD after(int n) {
+			if(n < 0) before(-n);
+			
+			YMD temp = new YMD(this.y, this.m, this.d);
+			
+			temp.d += n;
+			
+			while(temp.d > mdays[isLeap(temp.y)][temp.m-1]) {
+				temp.d -= mdays[isLeap(temp.y)][temp.m-1];
+				if(++temp.m > 12) {
+					temp.y++;
+					temp.m = 1;
+				}
+			}
+			
+			return temp;
+		}
+		
+		//n일 앞의 날짜를 반환
+		YMD before(int n) {
+			if(n < 0) after(-n);
+			
+			YMD temp = new YMD(this.y, this.m, this.d);
+			
+			temp.d -= n;
+			
+			while(temp.d < 1) {
+				if(--temp.m < 1) {
+					temp.y--;
+					temp.m = 12;
+				}
+				temp.d += mdays[isLeap(temp.y)][temp.m-1];
+			}
+			
+			return temp;
+		}
+	}
 	
 	public static int[][] mdays = {//각 달의 일수
 			{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
@@ -12,26 +61,7 @@ public class Exer_Q11 {
 	public static int isLeap(int year) {//서기 year년은 윤년인가?(윤년 : 1/평년 : 0)
 		return (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) ? 1 : 0;
 	}//isLeap
-	
-	int y;	//년
-	int m;	//월(1 ~ 12)
-	int d;	//일(1 ~ 31)
 		
-	Exer_Q11(int y, int m, int d) {//주어진 날짜로 설정
-		this.y = y;
-		this.m = m;
-		this.d = d;
-	}//생성자
-	
-	//n일 뒤의 날짜를 반환
-	Exer_Q11 after(int n) {
-		return null;
-	}
-	
-	//n일 앞의 날짜를 반환
-	Exer_Q11 before(int n) {
-		return null;
-	}
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -44,15 +74,15 @@ public class Exer_Q11 {
 		System.out.print("일：");
 		int d = sc.nextInt();
 		
-		Exer_Q11 date = new Exer_Q11(y, m, d);
+		YMD date = new YMD(y, m, d);
 		
 		System.out.print("몇 일 앞/뒤의 날짜를 구할까요? : ");
 		int n = sc.nextInt();
 		
-		Exer_Q11 d1 = date.after(n);
+		YMD d1 = date.after(n);
 		System.out.printf("%d일 뒤의 날짜는 %d년 %d월 %d일입니다.\n", n, d1.y, d1.m, d1.d);
 		
-		Exer_Q11 d2 = date.before(n);
-		System.out.printf("%d일 뒤의 날짜는 %d년 %d월 %d일입니다.\n", n, d2.y, d2.m, d2.d);
+		YMD d2 = date.before(n);
+		System.out.printf("%d일 전의 날짜는 %d년 %d월 %d일입니다.\n", n, d2.y, d2.m, d2.d);
 	}
 }
