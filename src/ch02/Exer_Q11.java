dd182bfb3dfb8b46f63885e5aaf33de3c330d204
alsrbxo0428 <alsrbxo0428@naver.com>
2021-06-21 23:12:21 +0900

    2021-06-21 클래스

--- src/ch02/Exer_Q11.java
+++ src/ch02/Exer_Q11.java
@@ -1,29 +1,58 @@
 package ch02;
 
+import java.util.Scanner;
+
 public class Exer_Q11 {
-	public static class YMD {
-		int y;	//년
-		int m;	//월(1 ~ 12)
-		int d;	//일(1 ~ 31)
+	
+	public static int[][] mdays = {//각 달의 일수
+			{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+			{31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+	};//mdays
+	
+	public static int isLeap(int year) {//서기 year년은 윤년인가?(윤년 : 1/평년 : 0)
+		return (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) ? 1 : 0;
+	}//isLeap
+	
+	int y;	//년
+	int m;	//월(1 ~ 12)
+	int d;	//일(1 ~ 31)
 		
-		YMD(int y, int m, int d) {
-			this.y = y;
-			this.m = m;
-			this.d = d;
-		}//생성자
-	}//YMD
+	Exer_Q11(int y, int m, int d) {//주어진 날짜로 설정
+		this.y = y;
+		this.m = m;
+		this.d = d;
+	}//생성자
 	
 	//n일 뒤의 날짜를 반환
-	public static void after(int n) {
-		
+	Exer_Q11 after(int n) {
+		return null;
 	}
 	
 	//n일 앞의 날짜를 반환
-	public static void before(int n) {
-		
+	Exer_Q11 before(int n) {
+		return null;
 	}
 	
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("날짜를 입력하세요.\n");
+		System.out.print("년：");
+		int y = sc.nextInt();
+		System.out.print("월：");
+		int m = sc.nextInt();
+		System.out.print("일：");
+		int d = sc.nextInt();
 		
+		Exer_Q11 date = new Exer_Q11(y, m, d);
+		
+		System.out.print("몇 일 앞/뒤의 날짜를 구할까요? : ");
+		int n = sc.nextInt();
+		
+		Exer_Q11 d1 = date.after(n);
+		System.out.printf("%d일 뒤의 날짜는 %d년 %d월 %d일입니다.\n", n, d1.y, d1.m, d1.d);
+		
+		Exer_Q11 d2 = date.before(n);
+		System.out.printf("%d일 뒤의 날짜는 %d년 %d월 %d일입니다.\n", n, d2.y, d2.m, d2.d);
 	}
 }
