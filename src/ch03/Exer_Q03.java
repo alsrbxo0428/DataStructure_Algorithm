615b25cea4b2e652c8a47ca1b754d3f5844bd5eb
alsrbxo0428 <alsrbxo0428@naver.com>
2021-06-27 21:44:30 +0900

    2021-06-27 검색알고리즘

---
+++ src/ch03/Exer_Q03.java
@@ -0,0 +1,30 @@
+package ch03;
+
+import java.util.Scanner;
+
+public class Exer_Q03 {
+	private static int searchIdx(int[] a, int n, int key, int[] idx) {
+		int cnt = 0;
+		
+		return cnt;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("요솟수 : ");
+		int n = sc.nextInt();
+		int[] idx = new int[n];
+		int[] x = new int[n];
+		
+		for(int i = 0; i < n; i++) {
+			System.out.print("x[" + i + "] : ");
+			x[i] = sc.nextInt();
+		}
+		
+		System.out.print("key : ");
+		int key = sc.nextInt();
+		
+		
+	}
+}
