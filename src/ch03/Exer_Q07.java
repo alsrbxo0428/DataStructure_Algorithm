a7d3c23e2e5e92aa9b36798988acadc81810eaff
alsrbxo0428 <alsrbxo0428@naver.com>
2021-06-30 00:24:30 +0900

    2021-06-30 검색알고리즘

--- src/ch03/Exer_Q07.java
+++ src/ch03/Exer_Q07.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Scanner;
 
-public class PhysExamSearch {
+public class Exer_Q07 {
 	static class PhyscData {
 		private String name;
 		private int height;
@@ -19,13 +19,13 @@ public class PhysExamSearch {
 		}//문자열을 반환하는 메서드(정보 확인용)
 		
 		//오름차순으로 정렬하기 위한 comparator
-		public static final Comparator<PhyscData> HEIGHT_ORDER
-			= new HeightOrderComparator();
+		public static final Comparator<PhyscData> VISION_ORDER
+			= new VisionOrderComparator();
 		
-		private static class HeightOrderComparator implements Comparator<PhyscData> {
+		private static class VisionOrderComparator implements Comparator<PhyscData> {
 			public int compare(PhyscData d1, PhyscData d2) {
-				return (d1.height > d2.height) ? 1 :
-					(d1.height < d2.height) ? -1 : 0;
+				return (d1.vision > d2.vision) ? 1 :
+						(d1.vision < d2.vision) ? -1 : 0;
 			}//compare
 		}//HeightOrderComparator
 	}//PhyscData
@@ -36,19 +36,19 @@ public class PhysExamSearch {
 		PhyscData[] x = {
 				new PhyscData("이나령", 162, 0.3),
 				new PhyscData("유지훈", 168, 0.4),
-				new PhyscData("김한결", 169, 0.8),
-				new PhyscData("홍준기", 171, 1.5),
 				new PhyscData("전서현", 173, 0.7),
+				new PhyscData("김한결", 169, 0.8),
 				new PhyscData("이호연", 174, 1.2),
+				new PhyscData("홍준기", 171, 1.5),
 				new PhyscData("이수민", 175, 2.0),
 		};
 		
-		System.out.print("몇 cm인 사람을 찾고 있나요? : ");
-		int height = sc.nextInt();
+		System.out.print("시력이 몇인 사람을 찾고 있나요? : ");
+		double vision = sc.nextDouble();
 		int idx = Arrays.binarySearch(
 				x,
-				new PhyscData("", height, 0.0),
-				PhyscData.HEIGHT_ORDER
+				new PhyscData("", 0, vision),
+				PhyscData.VISION_ORDER
 		);
 		
 		if(idx < 0)
