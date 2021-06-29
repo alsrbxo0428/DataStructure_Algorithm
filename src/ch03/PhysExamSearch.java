0fbc1bb1bd1c8dcba7ea0aee1dfef7b8666ae098
alsrbxo0428 <alsrbxo0428@naver.com>
2021-06-29 22:11:25 +0900

    2021-06-29 검색알고리즘

---
+++ src/ch03/PhysExamSearch.java
@@ -0,0 +1,13 @@
+package ch03;
+
+public class PhysExamSearch {
+	static class PhysData {
+		private String name;
+		private int height;
+		private double vision;
+		
+		public PhysData(String name, int height, double vision) {
+			this.name = name; this.height = height; this.vision = vision;
+		}
+	}
+}
